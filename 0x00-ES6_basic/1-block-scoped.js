f904de3bd506a98febffcc43f15a4d26a2dc0fb2
netnaho <87896136+netnaho@users.noreply.github.com>
2024-03-04 22:09:18 +0300

    another fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let taskAnother = false;
-    let task2Another = true;
-    taskAnother = task2Another;
-    task2Another = !taskAnother;
+    let task = false;
+    let task2 = true;
+    task = task2;
+    task2 = !task;
   }
 
   return [task, task2];
