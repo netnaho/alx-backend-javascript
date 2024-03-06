208094b9606ac523ab51da821842b5e11a24fd34
netnaho <87896136+netnaho@users.noreply.github.com>
2024-03-07 02:28:08 +0300

    minor fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
-import { uploadPhoto, createUser } from '../utils';
-
-const user = createUser();
-const photo = uploadPhoto();
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([photo, user])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
