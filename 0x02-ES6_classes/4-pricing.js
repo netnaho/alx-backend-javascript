a6f5957ff90af552e84841976f9d38d4e842c1f2
netnaho <87896136+netnaho@users.noreply.github.com>
2024-03-08 03:43:54 +0300

    minor fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency';
+import Currency from "./3-currency";
 
 const currency = new Currency();
 
@@ -25,7 +25,7 @@ export default class Pricing {
   }
 
   displayFulPrice() {
-    return `${this._amount} ${this._currency}_${this._currency.name} ${this._currency}_${this._currency.code}`;
+    return `${this._amount} ${this._currency}_${currency._name} ${this._currency}_${currency._code}`;
   }
 
   static convertPrice(amount, conversionRate) {
