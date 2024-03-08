ea2dbc962d23175407abc99f66104b7f35617d3a
netnaho <87896136+netnaho@users.noreply.github.com>
2024-03-08 03:40:12 +0300

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+const currency = new Currency();
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amt) {
+    this._amount = amt;
+  }
+
+  set currency(cur) {
+    this._currency = cur;
+  }
+
+  displayFulPrice() {
+    return `${this._amount} ${this._currency}_${currency._name} ${this._currency}_${currency._code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
